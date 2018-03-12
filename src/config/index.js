bf4263f241b0da3c8af07d7523cb9fb227273edb
andrewt929 <andrewt929@gmail.com>
2018-03-12 13:03:34 -0400

    Pulled in Class Project template

---
+++ src/config/index.js
@@ -0,0 +1,13 @@
+// src/config/index.js
+
+module.exports = {
+  appName: 'Our Glorious Node Project',
+  port: 3030,
+  db: {
+    username: 'andrewt929',
+    password: 'On5of5boys!',
+    host: 'ds239128.mlab.com:39128',
+    dbName: 'pottedplant',
+  }
+
+}
